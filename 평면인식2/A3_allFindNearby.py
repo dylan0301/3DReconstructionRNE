2f630e6aba3999e36c1cbbbab246582275f1df18
dylan0301 <87299340+dylan0301@users.noreply.github.com>
2022-11-18 06:36:11 +0900

    allfindnearby spatial tree 만들다 말았다

--- 평면인식2/A3_allFindNearby.py
+++ 평면인식2/A3_allFindNearby.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 from sklearn.metrics.pairwise import euclidean_distances
+import numpy as np
+import scipy.spatial as spatial
 
 def allFindNearby(AllPoints, hyperparameter):
     size = len(AllPoints)
@@ -13,3 +15,12 @@ def allFindNearby(AllPoints, hyperparameter):
         if numOfpts < 10:
             print('x: ', AllPoints[i].x, ' y: ', AllPoints[i].y, ' z: ', AllPoints[i].z)
             raise Exception('len(pts) < 10')
+    
+
+
+    # points = np.array([[p.x, p.y, p.z] for p in AllPoints.values()])
+    # point_tree = spatial.cKDTree(points)
+    # for i in range(size):
+    #     AllPoints[i].nearby1
+    #     nearby = point_tree.data[point_tree.query_ball_point([AllPoints[i].x,AllPoints[i].y, AllPoints[i].z],hyperparameter.R1)]
+        
--- 평면인식2/testst.py
+++ 평면인식2/testst.py
@@ -1,3 +1,8 @@
 import numpy as np
-iter = np.arange(0,20,1)
-print(iter)
+import scipy.spatial as spatial
+
+points = np.array([(1,2,3),(1,2,4),(1,2,5)])
+point_tree = spatial.cKDTree(points)
+
+print(point_tree.data[point_tree.query_ball_point([1,2,2],1.5)])
+# print(point_tree.data[point_tree.query_ball_point([1,2,2],2)])
