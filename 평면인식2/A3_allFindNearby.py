f91b2a1c9aa9e50c872dbe633d2454555c5ed84a
dylan0301 <87299340+dylan0301@users.noreply.github.com>
2022-11-18 20:18:00 +0900

    findnearby faster

--- 평면인식2/A3_allFindNearby.py
+++ 평면인식2/A3_allFindNearby.py
@@ -5,22 +5,28 @@ import scipy.spatial as spatial
 
 def allFindNearby(AllPoints, hyperparameter):
     size = len(AllPoints)
-    pointxyz = [[p.x, p.y, p.z] for p in AllPoints.values()]
-    distMat = euclidean_distances(pointxyz, pointxyz)
+    # pointxyz = [[p.x, p.y, p.z] for p in AllPoints.values()]
+    # distMat = euclidean_distances(pointxyz, pointxyz)
+    # for i in range(size):
+    #     for j in range(size):
+    #         if distMat[i][j] <= hyperparameter.R1:
+    #             AllPoints[i].nearby1.append(AllPoints[j]) #자기자신도 포함
+    #     numOfpts = len(AllPoints[i].nearby1)
+    #     if numOfpts < 4:
+    #         print('x: ', AllPoints[i].x, ' y: ', AllPoints[i].y, ' z: ', AllPoints[i].z)
+    #         raise Exception('len(pts) < 4')
+    
+    coorDict = dict()
+    for p in AllPoints.values():
+        coorDict[(p.x, p.y, p.z)] = p
+
+
+    points = np.array([[p.x, p.y, p.z] for p in AllPoints.values()])
+    point_tree = spatial.cKDTree(points)
     for i in range(size):
-        for j in range(size):
-            if distMat[i][j] <= hyperparameter.R1:
-                AllPoints[i].nearby1.append(AllPoints[j]) #자기자신도 포함
-        numOfpts = len(AllPoints[i].nearby1)
-        if numOfpts < 4:
+        nearby = point_tree.data[point_tree.query_ball_point([AllPoints[i].x,AllPoints[i].y, AllPoints[i].z],hyperparameter.R1)]
+        if len(nearby) < 4:
             print('x: ', AllPoints[i].x, ' y: ', AllPoints[i].y, ' z: ', AllPoints[i].z)
             raise Exception('len(pts) < 4')
-    
-
-
-    # points = np.array([[p.x, p.y, p.z] for p in AllPoints.values()])
-    # point_tree = spatial.cKDTree(points)
-    # for i in range(size):
-    #     AllPoints[i].nearby1
-    #     nearby = point_tree.data[point_tree.query_ball_point([AllPoints[i].x,AllPoints[i].y, AllPoints[i].z],hyperparameter.R1)]
-        
+        for q in nearby:
+            AllPoints[i].nearby1.append(coorDict[(q[0], q[1], q[2])])
--- 평면인식2/testst.py
+++ 평면인식2/testst.py
@@ -4,5 +4,5 @@ import scipy.spatial as spatial
 points = np.array([(1,2,3),(1,2,4),(1,2,5)])
 point_tree = spatial.cKDTree(points)
 
-print(point_tree.data[point_tree.query_ball_point([1,2,2],1.5)])
-# print(point_tree.data[point_tree.query_ball_point([1,2,2],2)])
+# print(point_tree.data[point_tree.query_ball_point([1,2,2],1.5)])
+print(point_tree.data[point_tree.query_ball_point([1,2,2],2)])
